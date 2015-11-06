4c75ac816fc560da69ee3f5e63ad78911d15f6bf
Nikolay Iliev <iliev.nick007@gmail.com>
2015-11-06 20:15:01 +0200

    add
    add

---
+++ RecursionWorkshop/NFibonachiWithMatrix/Startup.cs
@@ -0,0 +1,65 @@
+﻿namespace RecursionWorkshop
+{
+    using System;
+
+    /// <summary>
+    /// https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/blob/master/workshops/2015-11-06-recursion-and-data-structures/Fibonacci/README.md
+    /// </summary>
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+
+            var aMatrix = new FibMatrix(); // the initial fibonachi matrix 1, 1, 1, 0
+
+            Console.WriteLine(PowModeFibMatrix(aMatrix, n).Matrix[0, 1]);
+        }
+
+        public static FibMatrix PowModeFibMatrix(FibMatrix a, long p)
+        {
+            if (p == 1)
+            {
+                return a;
+            }
+
+            if (p % 2 == 1)
+            {
+                return new FibMatrix(PowModeFibMatrix(a, p - 1), a);
+            }
+
+            a = PowModeFibMatrix(a, p / 2);
+            return new FibMatrix(a, a);
+        }
+    }
+
+    public class FibMatrix
+    {
+        private const long MODULE = 1000000007;
+        public FibMatrix()
+        {
+            this.Matrix = new long[2, 2];
+            this.Matrix[0, 0] = 1;
+            this.Matrix[0, 1] = 1;
+            this.Matrix[1, 0] = 1;
+            this.Matrix[1, 1] = 0;
+        }
+
+        public FibMatrix(FibMatrix A, FibMatrix B)
+        {
+            this.Matrix = new long[2, 2];
+
+            this.Matrix[0, 0] = A.Matrix[0, 0] * B.Matrix[0, 0] + A.Matrix[0, 1] * B.Matrix[1, 0];
+            this.Matrix[0, 1] = A.Matrix[0, 0] * B.Matrix[0, 1] + A.Matrix[0, 1] * B.Matrix[1, 1];
+            this.Matrix[1, 0] = A.Matrix[1, 0] * B.Matrix[0, 0] + A.Matrix[1, 1] * B.Matrix[1, 0];
+            this.Matrix[1, 1] = A.Matrix[1, 0] * B.Matrix[0, 1] + A.Matrix[1, 1] * B.Matrix[1, 1];
+
+            this.Matrix[0, 0] %= MODULE;
+            this.Matrix[0, 1] %= MODULE;
+            this.Matrix[1, 0] %= MODULE;
+            this.Matrix[1, 1] %= MODULE;
+        }
+
+        public long[,] Matrix { get; set; }
+    }
+}
---
+++ RecursionWorkshop/Stools/Startup.cs
@@ -0,0 +1,134 @@
+﻿namespace Stools
+{
+    using System;
+
+    /// <summary>
+    /// https://github.com/TelerikAcademy/Data-Structures-and-Algorithms/blob/master/workshops/2015-11-06-recursion-and-data-structures/Stools/README.md
+    /// </summary>
+    public class Startup
+    {
+        private static Stool[] stools;
+
+        private static int n;
+
+        private static int[,,] memo;
+
+        public static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+
+            stools = new Stool[n];
+
+            memo = new int[1 << n, n, 3];
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                stools[i] = new Stool(
+                    int.Parse(line[0]), 
+                    int.Parse(line[1]), 
+                    int.Parse(line[2]));
+            }
+
+            int res = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                res = Math.Max(res, MaxHeight((1 << n) - 1, i, 0));
+                res = Math.Max(res, MaxHeight((1 << n) - 1, i, 1));
+                res = Math.Max(res, MaxHeight((1 << n) - 1, i, 2));
+            }
+          
+            Console.WriteLine(res);
+        }
+
+        // side x = 0 ; side y = 1; side x = 2;
+        public static int MaxHeight(int used, int top, int side)
+        {
+            if (memo[used, top, side] != 0)
+            {
+                return memo[used, top, side];
+            }
+
+            if (used == (1 << top))
+            {
+                if (side == 0)
+                {
+                    return stools[top].X;
+                }
+
+                if (side == 1)
+                {
+                    return stools[top].Y;
+                }
+
+                if (side == 2)
+                {
+                    return stools[top].Z;
+                }
+            }
+
+            // all stools without the TOP
+            int fromStools = used ^ (1 << top);
+
+            int sideX = (side == 0) ? stools[top].Y : stools[top].X;
+
+            int sideY = (side == 2) ? stools[top].Y : stools[top].Z;
+
+            int sideH = stools[top].X + stools[top].Y + stools[top].Z - sideX - sideY;
+
+            var res = sideH;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (((fromStools >> i) & 1) == 1)
+                {
+                    // stools[i] == 0
+                    if ((stools[i].Y >= sideX && stools[i].Z >= sideY) ||
+                        (stools[i].Y >= sideY && stools[i].Z >= sideX))
+                    {
+                        res = Math.Max(res, MaxHeight(fromStools, i, 0) + sideH);
+                    }
+
+                    // check if it is a cube
+                    if (stools[i].X == stools[i].Y && stools[i].Z == stools[i].Y)
+                    {
+                        continue;
+                    }
+
+                    // stools[i] == 1
+                    if ((stools[i].X >= sideX && stools[i].Z >= sideY) ||
+                        (stools[i].X >= sideY && stools[i].Z >= sideX))
+                    {
+                        res = Math.Max(res, MaxHeight(fromStools, i, 1) + sideH);
+                    }
+
+                    // stools[i] == 2
+                    if ((stools[i].X >= sideX && stools[i].Y >= sideY) ||
+                        (stools[i].X >= sideY && stools[i].Y >= sideX))
+                    {
+                        res = Math.Max(res, MaxHeight(fromStools, i, 2) + sideH);
+                    }
+                }
+            }
+            memo[used, top, side] = res;
+            return res;
+        }
+    }
+
+    public class Stool
+    {
+        public Stool(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public int X { get; set; }
+
+        public int Y{ get; set; }
+
+        public int Z { get; set; }
+    }
+}
